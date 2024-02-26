7f2a14a275b895055e5eae696382cf7bcb2e1e5b
yairnt <yair.nt@hotmail.com>
2024-02-26 13:46:56 -0300

    add cors

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require('express');
 const multer = require('multer');
 const path = require('path');
 const pool = require('./db');
-const app = express();
+const cors = require('cors');
 
+const app = express();
+app.use(cors());
 
 const storage = multer.diskStorage({
   destination: '/usr/src/app/images',
@@ -14,33 +16,33 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage });
 
-app.get('/images', async (req, res) => {
-    try {
-      const result = await pool.query('SELECT title, image FROM movies');
-  
-      const imagesData = result.rows.map(row => ({
-        imageUrl: row.image,
-        title: row.title
-      }));
-
-      res.json(imagesData);
-    } catch (error) {
-      console.error('Error al obtener las imágenes desde la base de datos:', error);
-      res.status(500).send('Error interno del servidor');
-    }
-  });
-
 app.post('/upload', upload.single('image'), async (req, res) => {
   try {
     const imageUrl = `/images/${req.file.filename}`;
     const { title } = req.body;
-    
+    console.info('antes de query', imageUrl, title)
     const result = await pool.query('INSERT INTO movies (title, image) VALUES ($1, $2) RETURNING *', [title, imageUrl]);
     console.log('result', result)
     res.json(result.rows[0]);
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'Error al cargar la imagen' });
+  }
+});
+
+app.get('/images', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT title, image FROM movies');
+
+    const imagesData = result.rows.map(row => ({
+      imageUrl: row.image,
+      title: row.title
+    }));
+
+    res.json(imagesData);
+  } catch (error) {
+    console.error('Error al obtener las imágenes desde la base de datos:', error);
+    res.status(500).send('Error interno del servidor');
   }
 });
 
