aff07dd09caee54cd5a92344b13ebe9ad5453b5d
yairnt <yair.nt@hotmail.com>
2024-02-27 16:46:49 -0300

    change hostname

--- db.js
+++ db.js
@@ -2,10 +2,10 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: 'user',
-  host: 'db', // Cambiado a 'db' que es el nombre del servicio en docker-compose.yml
-  database: 'database',
-  password: 'password',
+  user: 'movies_db_postgres_user',
+  host: 'dpg-cnf3m9779t8c73cebmeg-a', // Cambiado a 'db' que es el nombre del servicio en docker-compose.yml
+  database: 'movies_db_postgres',
+  password: 'LFib5cUEE9Cug2z7r3dE8RunHUMSUIGh',
   port: 5432,
 });
 
